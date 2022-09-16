af677af9a0582ab312a61dbd54e7fa32413a3488
tabassumara228 <tabassumara228@gmail.com>
2022-09-16 13:32:33 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,42 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta id="desc" name="description">
+    <meta id="key_words" name="keywords">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link async="true" rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/85e0b40181.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700i" rel="stylesheet">
+  <title>WhatsApp Web</title>
+  
+</head>
+<body>
+  <div class="whtback">
+     <div class="container-fluid white">
+     <div class="row">
+        <div class=" col-md-4 col-lg-4 left-side ">
+           <div class="">
+            In this article, I’ll tell you how I used React JS to create 
+           </div>
+        </div>
+        <div class=" col-md-8 col-lg-8 right-side">
+
+        </div>
+     </div>
+
+     </div>
+  </div>
+
+
+</body>
+</html> -->
